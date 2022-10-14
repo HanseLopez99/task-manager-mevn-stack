301d3d8a3792f0570885e63849b26dec4cb4c759
Hansel López <halm29799@gmail.com>
2022-10-14 12:42:57 -0600

    feat: create crud routes for task

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,8 +1,27 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('Tareas desde el servidor');
+const Task = require('../models/Task');
+
+router.get('/', async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+router.post('/', async (req, res) => {
+  const task = new Task(req.body);
+  await task.save();
+  res.status(200).send({ code: 1, status: 'Task Saved' });
+});
+
+router.put('/:id', async (req, res) => {
+  await Task.findByIdAndUpdate(req.params.id, req.body);
+  res.status(200).send({ code: 1, status: 'Task Updated' });
+});
+
+router.delete('/:id', async (req, res) => {
+  await Task.findByIdAndRemove(req.params.id);
+  res.status(200).send({ code: 1, status: 'Task Deleted' });
 });
 
 module.exports = router;
