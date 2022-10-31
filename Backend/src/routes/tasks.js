f84aa7caeaee158549b3e59ca2facb64b049e689
Hansel López <halm29799@gmail.com>
2022-10-31 11:16:38 -0600

    fix: routes not returning errors

--- Backend/src/routes/tasks.js
+++ Backend/src/routes/tasks.js
@@ -3,25 +3,65 @@ const router = Router();
 
 const Task = require('../models/Task');
 
+// GET all tasks
 router.get('/', async (req, res) => {
-  const tasks = await Task.find();
-  res.json(tasks);
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
+// GET one task
+router.get('/:id', async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    res.json(task);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// POST one task
 router.post('/', async (req, res) => {
-  const task = new Task(req.body);
-  await task.save();
-  res.status(200).send({ code: 1, status: 'Task Saved' });
+  try {
+    const task = new Task({
+      title: req.body.title,
+      description: req.body.description,
+    });
+    const savedTask = await task.save();
+    res.json({ message: 'Task saved', savedTask });
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
+// PUT one task
 router.put('/:id', async (req, res) => {
-  await Task.findByIdAndUpdate(req.params.id, req.body);
-  res.status(200).send({ code: 1, status: 'Task Updated' });
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      {
+        title: req.body.title,
+        description: req.body.description,
+      },
+      { new: true }
+    );
+    res.json({ message: 'Task updated', updatedTask });
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
+// DELETE one task
 router.delete('/:id', async (req, res) => {
-  await Task.findByIdAndRemove(req.params.id);
-  res.status(200).send({ code: 1, status: 'Task Deleted' });
+  try {
+    const removedTask = await Task.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Task deleted', removedTask });
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
 module.exports = router;
